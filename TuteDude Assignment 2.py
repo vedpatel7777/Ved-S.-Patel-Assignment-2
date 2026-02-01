91b399ab3d6233511c6458a8f6d9c88f8616e3c3
Ved Patel <ved20nagar@gmail.com>
2026-02-01 16:04:14 +0530

    Add files via upload

---
+++ TuteDude Assignment 2.py
@@ -0,0 +1,14 @@
+# Task 1
+number = int(input("Enter an integer: "))
+
+# Check if the number is even or odd
+if number % 2 == 0:
+    print(f"{number} is even.")
+else:
+    print(f"{number} is odd.")
+
+# Task 2
+total = 0
+for i in range(1, 51):
+    total += i
+print(f"The sum of integers from 1 to 50 is: {total}")
